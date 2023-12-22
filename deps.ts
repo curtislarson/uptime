5eea15741e69e9b49ca325e5d64b323ac1eafa4b
Curtis Larson <quackware@gmail.com>
2023-12-21 22:53:20 -0500

    feat: add ducky

--- deps.ts
+++ deps.ts
@@ -2,6 +2,7 @@ export {
   type FC,
   Fragment,
   jsx,
+  serveStatic,
 } from "https://deno.land/x/hono@v3.11.9/middleware.ts";
 export { Hono } from "https://deno.land/x/hono@v3.11.9/mod.ts";
 
--- src/server.tsx
+++ src/server.tsx
@@ -1,16 +1,22 @@
 /** @jsx jsx */
 /** @jsxFrag Fragment */
 
-import { Hono, jsx } from "../deps.ts";
+import { Hono, jsx, serveStatic } from "../deps.ts";
 import { Index } from "./index.tsx";
 import { UptimeKv } from "./kv.ts";
 import { logger } from "./logger.ts";
 import { UrlToCheck, CheckResponse } from "./types.ts";
 
+// This is called from the `main.ts` file in the parent directory so the path has
+// to be a reference from that file, not this `server.tsx` file
+const FAVICON_PATH = "./public/favicon.ico";
+
 export function createServer(kv: UptimeKv) {
   const app = new Hono();
 
   logger.info("Initializing server");
+
+  app.get("/favicon.ico", serveStatic({ path: FAVICON_PATH }));
 
   app.get("/", async (c) => {
     const urlToResponseMap = new WeakMap<UrlToCheck, CheckResponse[]>();
